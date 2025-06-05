9965f07575ebdba729000211b2315b0db4a97a4a
Moulik6235 <manochamoulik@gmail.com>
2025-06-05 11:33:13 +0530

    user login and logout function created

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -35,6 +35,19 @@ export const AppContextProvider = ({ children }) => {
         }
     }
 
+    // Fetch User Auth Status, User Data and Cart Items
+    const fetchUser = async ()=>{
+        try {
+            const {data} = await axios.get('api/user/is-auth');
+            if(data.success){
+                setUser(data.user)
+                setCartItems(data.user.cartItems)
+            }
+        } catch (error) {
+            setUser(null)
+        }
+    }
+
     // Fetch all products
     const fetchProducts = async () => {
        try {
@@ -84,9 +97,12 @@ export const AppContextProvider = ({ children }) => {
     }
 
     useEffect(() => {
+        fetchUser()
         fetchSeller()
         fetchProducts()
     }, [])
+
+    
 
     // Get Cart Item Count
     const getCartCount = () => {
